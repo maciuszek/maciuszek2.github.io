a692ce832058b311da37b08ad53be0a4e817194c
maciuszek <szczecinska.duma@gmail.com>
2023-12-20 16:38:26 -0500

    test pdf viewer with selectable text

--- js/minimal.js
+++ js/minimal.js
@@ -24,95 +24,82 @@
 //For reference, the actual PDF document that is rendered is available at:
 //http://vivin.net/pub/pdfjs/TestDocument.pdf
 
-window.onload = function () {
-    var scale = 1.5; //Set this to whatever you want. This is basically the "zoom" factor for the PDF.
+var { PDFJS } = globalThis;
 
-    /**
-     * Converts a base64 string into a Uint8Array
-     */
-    function base64ToUint8Array(base64) {
-        var raw = atob(base64); //This is a native function that decodes a base64-encoded string.
-        var uint8Array = new Uint8Array(new ArrayBuffer(raw.length));
-        for (var i = 0; i < raw.length; i++) {
-            uint8Array[i] = raw.charCodeAt(i);
+var scale = 1.5; //Set this to whatever you want. This is basically the "zoom" factor for the PDF.
+
+function loadPdf(pdfData) {
+    PDFJS.disableWorker = true; //Not using web workers. Not disabling results in an error. This line is
+    //missing in the example code for rendering a pdf.
+
+    var pdf = PDFJS.getDocument(pdfData);
+    pdf.then(renderPdf);
+}
+
+function renderPdf(pdf) {
+    pdf.getPage(1).then(renderPage);
+}
+
+function renderPage(page) {
+    var viewport = page.getViewport(scale);
+    var $canvas = jQuery("<canvas></canvas>");
+
+    //Set the canvas height and width to the height and width of the viewport
+    var canvas = $canvas.get(0);
+    var context = canvas.getContext("2d");
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    //Append the canvas to the pdf container div
+    var $pdfContainer = jQuery("#pdfContainer");
+    $pdfContainer.css("height", canvas.height + "px").css("width", canvas.width + "px");
+    $pdfContainer.append($canvas);
+
+    //The following few lines of code set up scaling on the context if we are on a HiDPI display
+    var outputScale = getOutputScale();
+    if (outputScale.scaled) {
+        var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
+            (1 / outputScale.sy) + ')';
+        CustomStyle.setProp('transform', canvas, cssScale);
+        CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
+
+        if ($textLayerDiv.get(0)) {
+            CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
+            CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
         }
-
-        return uint8Array;
     }
 
-    function loadPdf(pdfData) {
-        PDFJS.disableWorker = true; //Not using web workers. Not disabling results in an error. This line is
-        //missing in the example code for rendering a pdf.
-
-        var pdf = PDFJS.getDocument(pdfData);
-        pdf.then(renderPdf);
+    context._scaleX = outputScale.sx;
+    context._scaleY = outputScale.sy;
+    if (outputScale.scaled) {
+        context.scale(outputScale.sx, outputScale.sy);
     }
 
-    function renderPdf(pdf) {
-        pdf.getPage(1).then(renderPage);
-    }
-
-    function renderPage(page) {
-        var viewport = page.getViewport(scale);
-        var $canvas = jQuery("<canvas></canvas>");
-
-        //Set the canvas height and width to the height and width of the viewport
-        var canvas = $canvas.get(0);
-        var context = canvas.getContext("2d");
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        //Append the canvas to the pdf container div
-        var $pdfContainer = jQuery("#pdfContainer");
-        $pdfContainer.css("height", canvas.height + "px").css("width", canvas.width + "px");
-        $pdfContainer.append($canvas);
-
-        //The following few lines of code set up scaling on the context if we are on a HiDPI display
-        var outputScale = getOutputScale();
-        if (outputScale.scaled) {
-            var cssScale = 'scale(' + (1 / outputScale.sx) + ', ' +
-                (1 / outputScale.sy) + ')';
-            CustomStyle.setProp('transform', canvas, cssScale);
-            CustomStyle.setProp('transformOrigin', canvas, '0% 0%');
-
-            if ($textLayerDiv.get(0)) {
-                CustomStyle.setProp('transform', $textLayerDiv.get(0), cssScale);
-                CustomStyle.setProp('transformOrigin', $textLayerDiv.get(0), '0% 0%');
-            }
-        }
-
-        context._scaleX = outputScale.sx;
-        context._scaleY = outputScale.sy;
-        if (outputScale.scaled) {
-            context.scale(outputScale.sx, outputScale.sy);
-        }
-
-        var canvasOffset = $canvas.offset();
-        var $textLayerDiv = jQuery("<div />")
-            .addClass("textLayer")
-            .css("height", viewport.height + "px")
-            .css("width", viewport.width + "px")
-            .offset({
-                top: canvasOffset.top,
-                left: canvasOffset.left
-            });
-
-        $pdfContainer.append($textLayerDiv);
-
-        page.getTextContent().then(function (textContent) {
-            var textLayer = new TextLayerBuilder($textLayerDiv.get(0), 0); //The second zero is an index identifying
-            //the page. It is set to page.number - 1.
-            textLayer.setTextContent(textContent);
-
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport,
-                textLayer: textLayer
-            };
-
-            page.render(renderContext);
+    var canvasOffset = $canvas.offset();
+    var $textLayerDiv = jQuery("<div />")
+        .addClass("textLayer")
+        .css("height", viewport.height + "px")
+        .css("width", viewport.width + "px")
+        .offset({
+            top: canvasOffset.top,
+            left: canvasOffset.left
         });
-    }
 
-    loadPdf('./Matthew_Kuzminski.pdf');
-};
+    $pdfContainer.append($textLayerDiv);
+
+    page.getTextContent().then(function (textContent) {
+        var textLayer = new TextLayerBuilder($textLayerDiv.get(0), 0); //The second zero is an index identifying
+        //the page. It is set to page.number - 1.
+        textLayer.setTextContent(textContent);
+
+        var renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+            textLayer: textLayer
+        };
+
+        page.render(renderContext);
+    });
+}
+
+loadPdf('./Matthew_Kuzminski.pdf');
